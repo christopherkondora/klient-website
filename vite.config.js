a9c32303a971520b07bd77a452c8fec2347613f5
christopherkondora <christopherkondora@users.noreply.github.com>
2026-03-25 08:07:54 +0100

    feat: add reset-password page

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default {
       input: {
         main: resolve(__dirname, 'index.html'),
         docs: resolve(__dirname, 'docs.html'),
+        'reset-password': resolve(__dirname, 'reset-password.html'),
       },
     },
   },
